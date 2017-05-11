64e1a7c905555cd47e66a01401fe39473e6f800c
kenwest <ken.west@internode.on.net>
2017-05-11 22:23:12 +1000

    Make the collapsible menu pop up over the header

--- templates/page.tpl.php
+++ templates/page.tpl.php
@@ -74,7 +74,7 @@
  */
 ?>
 <header id="navbar" role="banner" class="<?php print $navbar_classes; ?>">
-  <div class="<?php print $container_class; ?>">
+  <div class="container">
     <div class="navbar-header">
       <?php if ($logo): ?>
         <a class="logo navbar-btn pull-left" href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>">
