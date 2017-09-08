bf0912c4c38943f03c9a9f7ff466763ff8e36208
kenwest <ken.west@internode.on.net>
2017-09-08 16:36:26 +1000

    Break the collapsible navbar into 3: primary menu, city menu and search. Add JavaScript to collapse other navbars when a navbar is opened. Make the 3 navbar icons responsive.

--- template.php
+++ template.php
@@ -22,13 +22,14 @@ function reaching_preprocess_html(&$variables) {
 
 
 /*
- * Limit the height of blocks
+ * Limit the height of blocks, unless they are "unbounded".
+ * Collapse other navbar-collapse DIVs when a navbar-toggle is pressed.
  */
 function reaching_preprocess_page(&$variables) {
   $script = '
     jQuery(document).ready(function(){
       jQuery(".region-content, .region-sidebar-first, .region-sidebar-second").find(".block-views, .block-block").each(function() {
-        if(jQuery(this).height() >= 400 && !jQuery(this).hasClass("unbounded")) {
+        if (jQuery(this).height() >= 400 && !jQuery(this).hasClass("unbounded")) {
           jQuery(this).toggleClass("bounded");
           jQuery("<div class=\"block-header\"></div>").prependTo(jQuery(this));
           jQuery("<div class=\"block-footer\"><i class=\"fa fa-fw fa-chevron-down\"></div>").appendTo(jQuery(this)).click(function() {
@@ -36,6 +37,18 @@ function reaching_preprocess_page(&$variables) {
             jQuery(this).find("i").toggleClass("fa-rotate-180");
           });
         }
+      });
+      jQuery(".navbar-toggle").click(function() {
+        var target = jQuery(this).attr("data-target");
+        jQuery(".navbar-toggle").each(function () {
+          var other = jQuery(this).attr("data-target");
+          if (other != target) {
+            jQuery(this).toggleClass("collapsed", true);
+            jQuery(this).attr("aria-expanded", "false")
+            jQuery(other).toggleClass("in", false);
+            jQuery(other).attr("aria-expanded", "false")
+          }
+        });
       });
     });';
   drupal_add_js($script, 'inline');
--- templates/page.tpl.php
+++ templates/page.tpl.php
@@ -93,27 +93,51 @@
         <a class="name navbar-brand" href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>"><?php print $site_name; ?></a>
       <?php endif; ?>
 
-      <?php if (!empty($primary_nav) || !empty($secondary_nav) || !empty($page['navigation'])): ?>
-        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar-collapse">
-          <span class="sr-only"><?php print t('Toggle navigation'); ?></span>
-          <i class="fa fa-fw fa-bars"></i>
-        </button>
-      <?php endif; ?>
+      <div class="navbar-toggles">
+        <?php if (!empty($primary_nav)): ?>
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar-collapse-primary">
+            <span class="sr-only"><?php print t('Toggle primary navigation'); ?></span>
+            <i class="fa fa-fw fa-bars"></i>
+          </button>
+        <?php endif; ?>
+
+        <?php if (!empty($secondary_nav)): ?>
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar-collapse-secondary">
+            <span class="sr-only"><?php print t('Toggle city navigation'); ?></span>
+            <i class="fa fa-fw fa-map-marker"></i>
+          </button>
+        <?php endif; ?>
+
+        <?php if (!empty($page['navigation'])): ?>
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar-collapse-search">
+            <span class="sr-only"><?php print t('Toggle search'); ?></span>
+            <i class="fa fa-fw fa-search"></i>
+          </button>
+        <?php endif; ?>
+      </div>
     </div>
 
-    <?php if (!empty($primary_nav) || !empty($secondary_nav) || !empty($page['navigation'])): ?>
-      <div class="navbar-collapse collapse" id="navbar-collapse">
+    <?php if (!empty($primary_nav)): ?>
+      <div class="navbar-collapse collapse" id="navbar-collapse-primary">
         <nav role="navigation">
-          <?php if (!empty($primary_nav)): ?>
-            <?php print render($primary_nav); ?>
-          <?php endif; ?>
-          <?php if (!empty($secondary_nav)): ?>
-            <?php print render($secondary_nav); ?>
-          <?php endif; ?>
-          <?php if (!empty($page['navigation'])): ?>
-            <?php print render($page['navigation']); ?>
-          <?php endif; ?>
-        </nav>
+          <?php print render($primary_nav); ?>
+       </nav>
+      </div>
+    <?php endif; ?>
+
+    <?php if (!empty($secondary_nav)): ?>
+      <div class="navbar-collapse collapse" id="navbar-collapse-secondary">
+        <nav role="navigation">
+          <?php print render($secondary_nav); ?>
+       </nav>
+      </div>
+    <?php endif; ?>
+
+    <?php if (!empty($page['navigation'])): ?>
+      <div class="navbar-collapse collapse" id="navbar-collapse-search">
+        <nav role="navigation">
+          <?php print render($page['navigation']); ?>
+       </nav>
       </div>
     <?php endif; ?>
   </div>
