8d37c3f69c0f9b9aabe268c83971ed024c7c2a57
kenwest <ken.west@internode.on.net>
2017-10-05 11:05:15 +1100

    When standardising the height of elements, use outerHeight to account for padding and borders

--- template.php
+++ template.php
@@ -31,7 +31,7 @@ function reaching_preprocess_page(&$variables) {
     jQuery(document).ready( function() {
       function standardiseHeight(elements) {
         elements.each( function() {
-          var height = jQuery(this).height();
+          var height = jQuery(this).outerHeight(false);
           var fraction = height % 20;
           if (jQuery(this).css("font-size") !== "14px" && fraction !== 0) {
             jQuery(this).css("min-height",  height - fraction + 20);
